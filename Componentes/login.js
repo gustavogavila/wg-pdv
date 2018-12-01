1b1720410ec2c16cd721834ba2a428c866fb6d52
Wanderson <anderson_alar@outlook.com>
2018-12-01 21:07:23 -0200

    o.O

--- Componentes/login.js
+++ Componentes/login.js
@@ -28,14 +28,12 @@ class Login extends Componente {
     <div class="login-pdv">
        <img src='./imgs/tapioca.png' class='logo'/>
        <h1 class='login-titulo'>Login</h1>
-        <form #onSubmit=logar()>
         <p>Usuario</p>
         <input class="campo" id='usu' type='text' placeholder='Usuario'/>
         <p>Senha</p>
         <input class="campo" id='senha' type='password' placeholder='Senha'/>
-        <button class="btn btn-login" type="submit" >Logar</button> 
+        <button class="btn btn-login"  #onSubmit=logar() >Logar</button> 
         <button type="button" class="btn btn-login" #onclick=recuperarSenha() >Esqueci minha senha</button>
-        </form>
         </div>
         `;
   }
